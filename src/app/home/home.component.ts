9c25c7b4a245d5d03f26f6017455c79ec63b8ee8
Michael Brackett <34899426+MLJBrackett@users.noreply.github.com>
2020-07-01 23:51:55 -0400

    added home html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import Typed from 'typed.js';
+import * as $ from "jquery";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +13,28 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+    let options = {
+      strings: ['a tech enthusiast.', 'your future employee.', 'your favourite employee.', 'a future CEO.', 'a hard worker.','a quick learner.','constantly improving.'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true,
+      smartBackspace: true,
+      backDelay:300
+    };
+    
+    let typed = new Typed('#typed-element', options);
+
+    $("#btn-group > .btn").click(function(){
+      $("#btn-group > .btn").removeClass("active");
+      $(this).addClass("active");
+    });
+  }
+  
+  showDiv={
+    parttime : true,
+    seneca :false,
+    mgcs :false,
+    youremp :false
+  }
 }
